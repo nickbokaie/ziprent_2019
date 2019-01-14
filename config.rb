b962129910a3a8692fec3b1072de6078e781d285
Nicholas Bokaie <nicholasbokaie@Nicholass-MacBook-Pro.local>
2019-01-14 14:41:42 -0800

    First commit

---
+++ config.rb
@@ -0,0 +1,27 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+# Livereload
+activate :livereload
+
+# Pretty URLs
+activate :directory_indexes
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+configure :build do
+  activate :minify_css
+  activate :minify_html
+end
+
+
