b79ba7a37a8810fec24075808b4724ff12b420fc
Nicholas Bokaie <nicholasbokaie@nicholass-mbp.lan>
2019-07-05 16:49:45 -0700

    testing area specific page structure

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,4 @@
-// This is where it all goes :)
+// Responsive Navigation
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
@@ -19,3 +19,37 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+// Show an element
+var show = function (elem) {
+	elem.classList.add('is-visible');
+};
+
+// Hide an element
+var hide = function (elem) {
+	elem.classList.remove('is-visible');
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+	elem.classList.toggle('is-visible');
+};
+
+// Listen for click events
+document.addEventListener('click', function (event) {
+
+	// Make sure clicked element is our toggle
+	if (!event.target.classList.contains('toggle')) return;
+
+	// Prevent default link behavior
+	event.preventDefault();
+
+	// Get the content
+	var content = document.querySelector(event.target.hash);
+	if (!content) return;
+
+	// Toggle the content
+	toggle(content);
+
+}, false);
+
